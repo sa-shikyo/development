bb8d1cee87f20b7a31966b22c83b1e8b257154f0
sa-shikyo <chzuozhiqiang@sohu.com>
2017-07-06 00:09:46 +0800

    111

--- website/sfs/views/sfs.py
+++ website/sfs/views/sfs.py
@@ -8,18 +8,46 @@ from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
 
 from sfs.models import SfsManagermsg
+from sfs.models import SfsTopic
+from sfs.models import SfsCompany
 
 
 @require_http_methods(["GET", "POST"])
 def sfs_homepage(request):
-    today = date.today()
-    managermsg = SfsManagermsg.object.filter(apply_begin=<today).first()
-    return render(request, 'sfs/homepage.html', {'managermsg': managermsg,})
+    """
+    ホームページ
+    """
+
+    # ------------------------
+    # 管理者のメッセージの取得
+    # ------------------------
+    # today = date.today()
+    managermsg = SfsManagermsg.objects.filter().first()
+
+    # ------------------
+    # トピック情報の取得
+    # ------------------
+    topic = SfsTopic.objects.filter().all()
+
+    return render(request, 'sfs/homepage.html', {
+    	'managermsg': managermsg,
+    	'topic': topic,
+    })
 
 
 @require_http_methods(["GET", "POST"])
 def sfs_company_message(request):
-    return render(request,'sfs/company_message.html', {})
+	"""
+    社会情報
+	"""
+
+    # --------------
+    # 社会情報の取得
+    # --------------
+    company_info = SfsCompany.objects.filter().all()
+    return render(request,'sfs/company_message.html', {
+        'company_info': company_info,
+    })
 
 
 @require_http_methods(["GET", "POST"])
